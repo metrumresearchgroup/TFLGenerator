191ac4009eedecd9de09ddffadd919c1973a3eba
Dan Polhamus <danp@metrumrg.com>
2016-04-18 20:29:26 +0000

    NMTab working now for R script output

--- script/ui.R
+++ script/ui.R
@@ -24,7 +24,7 @@ library(shinyFiles)
       	      h1(""),
       	      # shinyDirButton(id="dataPath", label='Set working directory', title='Please select a working directory'),
       	      h1(""),
-      	      menuItem(text="The Amgen TFL Generator", tabName = "tabIntro", icon=icon("clock")),
+      	      menuItem(text="Pharmacometrics TFL Generator", tabName = "tabIntro", icon=icon("clock")),
       	      menuItem(text="Data Input", tabName="tabDataInput", icon=icon("sliders")),
       	      menuItem(text="Data", tabName="tabData", icon=icon("database")),
       	      menuItem(text="Analysis Selection", tabName="tabAnalysisSelection", icon=icon("check-square-o")),
