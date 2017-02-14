6572b6ab6e969a626eb9d505c6bd60eec200d799
yonicd <yonicd@gmail.com>
2017-02-14 15:19:48 -0500

    add gglsave to TFL

--- script/0069source.R
+++ script/0069source.R
@@ -1,8 +1,8 @@
 library(dplyr)
-tab=read.table("NMStorage/0069/0069.tab", header=TRUE, skip=1, stringsAsFactors=F, fill=TRUE)
+dataDir='../TFL/inst/external/0069'
+tab=read.table(file.path(dataDir,"0069.tab"), header=TRUE, skip=1, stringsAsFactors=F, fill=TRUE)
 tab <- subset(tab, select=c(ID:HGTB,MDV))
 tab <- group_by(tab, ID,TIME,STUD,EVID)
-summary(tab)
 tab <- mutate(tab, WGT=WGTB+.1*TIME+rnorm(length(WGTB)))
 tab$WGT[ tab$STUD==183 ] <- NA
-write.csv(tab, file="NMStorage/0069/source.csv",row.names=F)
+write.csv(tab, file=file.path(dataDir,'0069.csv'),row.names=F)
