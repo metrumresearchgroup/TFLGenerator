6f20759cc243e7a7d78e24bcb93812aef0c73b45
jonathans <yonicd@gmail.com>
2017-04-21 10:37:27 -0400

    update package

--- TFLBook/buildChapters.R
+++ TFLBook/buildChapters.R
@@ -1,3 +1,3 @@
 TOC=queryChapter(concept = c('table','figure'))
-createChapter('Tables','01',funs = list(name=TOC$Name[TOC$output=='table'][4:1],pkg='TFL',type='table'))
+#createChapter('Tables','01',funs = list(name=TOC$Name[TOC$output=='table'][4:1],pkg='TFL',type='table'))
 createChapter('Figures','02',funs = list(name=TOC$Name[TOC$output=='figure'],pkg='TFL',type='figure'))
