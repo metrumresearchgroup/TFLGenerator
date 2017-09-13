a528ce7a242c6ed7077c15dc245fba5283b65182
Dan Polhamus <danp@metrumrg.com>
2017-09-13 20:18:21 +0000

    nginx fixes (v2.1 tested)

--- script/nginx_update.sh
+++ script/nginx_update.sh
@@ -2,8 +2,12 @@
 
 # Run as root
 service nginx stop
-apt-get install -y python-software-properties
-add-apt-repository ppa:nginx/stable
+apt-get autoremove nginx
+echo "deb http://nginx.org/packages/ubuntu/ precise nginx" >> /etc/apt/sources.list
+echo "deb-src http://nginx.org/packages/ubuntu/ precise nginx" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
 apt-get update
-aptitude safe-upgrade -y nginx
+apt-get install nginx
+cp nginx/nginx.conf /etc/nginx
+cp nginx/metworx /etc/nginx/sites-available
 service nginx restart
