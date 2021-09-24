436838979e28a6d49508f4ef20f9fe2b4b6d62fc
JohnSalter <jsalter@ip-10-242-143-234.us-west-2.compute.internal>
2021-09-24 12:17:14 -0400

    change: changing make_local_repo.R script to work with a version of Dr Drat that is compatible with R3.5.3

--- script/make_local_repo.R
+++ script/make_local_repo.R
@@ -12,7 +12,7 @@
 
 ## We need drat to be installed, which is not part of TFL's dependencies, so we're doing it separately.
 ## You need a fairly recent (as of 07-2021) version of drat for this. I did it with 0.2.1. This MPN Snapshot has 0.2.0, which is probably fine?
-install.packages("drat", repos = "https://mpn.metworx.com/snapshots/stable/2021-06-20")
+install.packages("drat", repos = "https://mpn.metworx.com/snapshots/stable/2020-03-24")
 
 ## Compile a list of all tarballs downloaded during the pkgr install process.
 toAdd1 <- list.files("tfl-pkgr-cache/MRAN-3f9ff60f0b67/src/", full.names = T)
@@ -24,4 +24,9 @@ repo_path <- "/usr/local/repos/tfl"
 
 ## Add the packages from pkgr's cache to the local repository.
 ## This repository will be accessible by RSConnect.
-drat::insertPackages(toAdd, repodir=repo_path)
+# drat::insertPackages(toAdd, repodir=repo_path) # Not compatible with drat versions that are compatible with R3.5.3
+for(pkg in toAdd) {
+  print(paste0("adding package ", pkg, " to local repo."))
+  drat::insertPackage(pkg, repodir=repo_path)
+}
+
